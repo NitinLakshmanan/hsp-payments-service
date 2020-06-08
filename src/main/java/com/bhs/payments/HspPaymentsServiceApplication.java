5ed82ec9114634c1e61ee9241d5b51625671c410
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2020-06-08 18:37:41 +0530

    Payments | Akhil | Format classes for code style

--- src/main/java/com/bhs/payments/HspPaymentsServiceApplication.java
+++ src/main/java/com/bhs/payments/HspPaymentsServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HspPaymentsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HspPaymentsServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HspPaymentsServiceApplication.class, args);
+  }
 
 }
