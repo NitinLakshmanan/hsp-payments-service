c17f34ac555ae471061b5a52d1b465c2a3c7e01f
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2020-06-08 14:50:27 +0530

    Payments | Akhil | Initial project setup for payments

---
+++ src/main/java/com/bhs/payments/HspPaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bhs.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspPaymentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspPaymentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhs/payments/HspPaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhs.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HspPaymentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
